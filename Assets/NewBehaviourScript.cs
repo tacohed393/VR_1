3567097b2b5048939f9d7803f37e3df3f7375c7e
tacohed393 <valverine393@gmail.com>
2023-10-31 19:31:57 +0300

    1
    1

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ToggleObject : MonoBehaviour
+{
+    public GameObject targetObject;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (targetObject.activeSelf)
+            {
+                targetObject.SetActive(false);
+            }
+            else
+            {
+                targetObject.SetActive(true);
+            }
+        }
+    }
+}
